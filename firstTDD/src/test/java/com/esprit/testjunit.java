19f739492e40ac0cd75817a3d63c069d17e7b693
aymengit7 <aimenbalti@gmail.com>
2015-12-02 11:04:46 +0100

    junit test balti
    Signed-off-by: aymengit7 <aimenbalti@gmail.com>

---
+++ firstTDD/src/test/java/com/esprit/testjunit.java
@@ -0,0 +1,14 @@
+package com.esprit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testjunit {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
